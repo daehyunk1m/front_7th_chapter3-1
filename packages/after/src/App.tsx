0351bd770b5666e4266ddb3ea24825213e2a394e
daehyun kim <101967272+daehyunk1m@users.noreply.github.com>
2025-11-25 22:59:30 +0900

    Add: tailwindCSS 추가 및 vite.config에 설정 추가

--- packages/after/src/App.tsx
+++ packages/after/src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import { Header } from './components/organisms'
-import { ManagementPage } from './pages/ManagementPage'
-import './styles/components.css'
+import React from "react";
+import { Header } from "./components/organisms";
+import { ManagementPage } from "./pages/ManagementPage";
+import "./styles/components.css";
 
 export const App: React.FC = () => {
   return (
-    <div style={{ minHeight: '100vh', backgroundColor: '#f7fafc' }}>
+    <div style={{ minHeight: "100vh", backgroundColor: "#f7fafc" }}>
       <Header />
       <main>
+        <div className='bg-blue-500 text-white p-4'>test</div>
         <ManagementPage />
       </main>
     </div>
